11b23dfdf652f8b934421aafc2c651fdcc7af37e
Jack Martin <jackmartin.dev@gmail.com>
2024-08-04 12:27:36 +0900

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <p>Hi</p>
+      <button className="btn btn-error">Click me</button>
+      </div>
+    </main>
+  );
+}
